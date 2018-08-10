c0fe70e94c6051d11fa67cff905111d6ccf0292a
Dustin Starke <dustin.starke@gmail.com>
2018-08-10 12:01:16 -0700

    style text using css and create beginning logic for jQuery

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(event) {
+  event.preventDefault();
+  var answer1 = $("input:radio[name=question1]:checked").val();
+  var answer2 = $("input:radio[name=question2]:checked").val();
+  var answer3 = $("input:radio[name=question3]:checked").val();
+  var answer4 = $("input:radio[name=question1]:checked").val();
+  var answer5 = $("input:radio[name=question1]:checked").val();
+
+  var php = 0;
+  var ruby = 0;
+  var csharp = 0;
+  var java = 0;
+
+  if (answer1 === "")});
