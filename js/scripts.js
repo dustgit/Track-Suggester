c5eff80a5f5d4146d7ce655bbd82de612638750d
Dustin Starke <dustin.starke@gmail.com>
2018-08-10 13:39:11 -0700

    assign comparative operators to variables to determine output value

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,90 @@
-$(document).ready(function(event) {
-  event.preventDefault();
-  var answer1 = $("input:radio[name=question1]:checked").val();
-  var answer2 = $("input:radio[name=question2]:checked").val();
-  var answer3 = $("input:radio[name=question3]:checked").val();
-  var answer4 = $("input:radio[name=question1]:checked").val();
-  var answer5 = $("input:radio[name=question1]:checked").val();
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    var answer1 = $("input:radio[name=question1]:checked").val();
+    var answer2 = $("input:radio[name=question2]:checked").val();
+    var answer3 = $("input:radio[name=question3]:checked").val();
+    var answer4 = $("input:radio[name=question1]:checked").val();
+    var answer5 = $("input:radio[name=question1]:checked").val();
 
-  var php = 0;
-  var ruby = 0;
-  var csharp = 0;
-  var java = 0;
+    var php = 0;
+    var ruby = 0;
+    var csharp = 0;
+    var java = 0;
 
-  if (answer1 === "csharp") {
-    csharp++;
-  } else if (answer1 === "java") {
-    java++;
-  } else if (answer1 === "ruby") {
-    ruby++;
-  } else {
-    php++;
-  }
+    console.log(php, ruby, csharp, java);
 
-  if (answer2 === "csharp") {
-    csharp++;
-  } else if (answer2 === "java") {
-    java++;
-  } else if (answer2 === "ruby") {
-    ruby++;
-  } else {
-    php++;
-  }
 
-  if (answer3 === "csharp") {
-    csharp++;
-  } else if (answer3 === "java") {
-    java++;
-  } else if (answer3 === "ruby") {
-    ruby++;
-  } else {
-    php++;
-  }
+    if (answer1 === "csharp") {
+      csharp++;
+    } else if (answer1 === "java") {
+      java++;
+    } else if (answer1 === "ruby") {
+      ruby++;
+    } else {
+      php++;
+    }
 
-  if (answer4 === "csharp") {
-    csharp++;
-  } else if (answer4 === "java") {
-    java++;
-  } else if (answer4 === "ruby") {
-    ruby++;
-  } else {
-    php++;
-  }
+    if (answer2 === "csharp") {
+      csharp++;
+    } else if (answer2 === "java") {
+      java++;
+    } else if (answer2 === "ruby") {
+      ruby++;
+    } else {
+      php++;
+    }
 
-  if (answer5 === "csharp") {
-    csharp++;
-  } else if (answer5 === "java") {
-    java++;
-  } else if (answer5 === "ruby") {
-    ruby++;
-  } else {
-    php++;
-  }
+    if (answer3 === "csharp") {
+      csharp++;
+    } else if (answer3 === "java") {
+      java++;
+    } else if (answer3 === "ruby") {
+      ruby++;
+    } else {
+      php++;
+    }
 
-  if (csharp >= java && csharp >= ruby && csharp >=php) {
-    $(".csharp").show();
-    $(".java").hide();
-    $(".ruby").hide();
-    $(".php").hide();
-  } else if (java >= csharp && java >= ruby && java >=php) {
-    $(".csharp").hide();
-    $(".java").show();
-    $(".ruby").hide();
-    $(".php").hide();
-  }else if (ruby >= csharp && ruby >= java && ruby >=php) {
-    $(".csharp").hide();
-    $(".java").hide();
-    $(".ruby").show();
-    $(".php").hide();
-  }else if (php >= csharp && php >= java & php >= ruby)
-    $(".csharp").show();
-    $(".java").hide();
-    $(".ruby").hide();
-    $(".php").hide();
-});
+    if (answer4 === "csharp") {
+      csharp++;
+    } else if (answer4 === "java") {
+      java++;
+    } else if (answer4 === "ruby") {
+      ruby++;
+    } else {
+      php++;
+    }
+
+    if (answer5 === "csharp") {
+      csharp++;
+    } else if (answer5 === "java") {
+      java++;
+    } else if (answer5 === "ruby") {
+      ruby++;
+    } else {
+      php++;
+    }
+
+    if (csharp >= java && csharp >= ruby && csharp >=php) {
+      $("#csharp").show();
+      $("#java").hide();
+      $("#ruby").hide();
+      $("#php").hide();
+    } else if (java >= csharp && java >= ruby && java >=php) {
+      $("#csharp").hide();
+      $("#java").show();
+      $("#ruby").hide();
+      $("#php").hide();
+    } else if (ruby >= csharp && ruby >= java && ruby >=php) {
+      $("#csharp").hide();
+      $("#java").hide();
+      $("#ruby").show();
+      $("#php").hide();
+    } else (php >= csharp && php >= java & php >= ruby)
+      $("#csharp").hide();
+      $("#java").hide();
+      $("#ruby").hide();
+      $("#php").show();
+    })
+    console.log();
+  });
